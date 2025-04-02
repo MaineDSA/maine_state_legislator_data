82ac161671229a6ea2605c4485eafcfbfaa067a7
Wil T <wil.t.me@pm.me>
2025-04-02 19:18:50 -0400

    get full list of pages and download data from all

--- src/scraper.py
+++ src/scraper.py
@@ -2,8 +2,9 @@ import re
 from pathlib import Path
 from urllib.parse import ParseResult
 
+import ratelimit
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import csv
 
 from tqdm import tqdm
@@ -29,6 +30,8 @@ def extract_legislator_from_string(text: str) -> tuple[str, str, str, str]:
     return district, town, member, party
 
 
+@ratelimit.sleep_and_retry
+@ratelimit.limits(calls=12, period=10)
 def parse_legislators_page(url: ParseResult, value: str, query: str = "selectedLetter") -> list[tuple[str, str, str, str]]:
     page_url = url
     page_url = page_url._replace(query=f"{query}={value}")
@@ -36,21 +39,33 @@ def parse_legislators_page(url: ParseResult, value: str, query: str = "selectedL
     response = requests.get(page_url.geturl(), allow_redirects=True)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    table = soup.find("table", class_="short-table")
-    if not table:
+    table_tag = soup.find("table", class_="short-table")
+    if not table_tag or not isinstance(table_tag, Tag):
         return []
 
-    rows = table.find_all("tr")[2:]  # Skip first 2 rows (header)
+    legislators: list = []
+    for table_row_tag in table_tag.find_all("tr")[2:]:  # Skip first 2 rows (header)
+        table_cell = table_row_tag.find("td", class_="short-tabletdlf")
+        extract_legislator_from_string(table_cell.get_text())
 
-    return [extract_legislator_from_string(row.find("td", class_="short-tabletdlf").get_text()) for row in rows]
+    return legislators
+
+
+def get_pagination(url: ParseResult) -> list[str]:
+    response = requests.get(url.geturl(), allow_redirects=True)
+    soup = BeautifulSoup(response.content, "html.parser")
+    pages = soup.find("ul", class_="pagination")
+    if not pages or not isinstance(pages, Tag):
+        return []
+
+    return [page.get_text() for page in pages.find_all("a")]
 
 
 def main() -> None:
     url = ParseResult(scheme="https", netloc="legislature.maine.gov:443", path="/house/house/MemberProfiles/ListAlphaTown", params="", query="", fragment="")
 
-    letters = ["A", "B"]
-
-    pages = [parse_legislators_page(url, letter) for letter in tqdm(letters)]
+    letters = get_pagination(url)
+    pages = [parse_legislators_page(url, letter) for letter in tqdm(letters, unit="pages")]
 
     with Path("district_data.csv").open(mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
